474e0e027c0d16462983b5f2374904c591411a1e
Sarah <xsarahyu@gmail.com>
2024-02-09 13:56:08 -0500

    Remove multer line

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const upload = require('../middleware/multer')
 const profileController = require('../controllers/profile')
 const { ensureAuth } = require('../middleware/auth')
 
