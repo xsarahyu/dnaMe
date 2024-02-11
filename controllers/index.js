b4804bae21e30a4502c791f60bb60d7aec2d3bf2
Sarah <xsarahyu@gmail.com>
2024-02-11 13:53:01 -0500

    Put getIndex and getHome in home controller

--- controllers/index.js
+++
@@ -1,7 +0,0 @@
-// controllers/indexController.js
-
-module.exports = {
-  getIndex: (req, res) => {
-    res.render('index.ejs')
-  }
-}
--- routes/main.js
+++ routes/main.js
@@ -1,20 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/auth')
-const indexController = require('../controllers/index')
-const userController = require('../controllers/user')
+const homeController = require('../controllers/home')
 const { ensureAuth } = require('../middleware/auth')
 
-// Main Routes
-router.get('/', indexController.getIndex)
-router.get('/home', ensureAuth, userController.getHome)
+router.get('/', homeController.getIndex)
+
+router.get('/signup', authController.getSignup)
+router.post('/signup', authController.postSignup)
 
 router.get('/login', authController.getLogin)
 router.post('/login', authController.postLogin)
 
-router.get('/logout', userController.logout)
+router.get('/home', ensureAuth, homeController.getHome)
 
-router.get('/signup', authController.getSignup)
-router.post('/signup', authController.postSignup)
+router.get('/logout', authController.logout)
 
 module.exports = router
